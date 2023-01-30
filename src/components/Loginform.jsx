44136a70b9e763519fbf71a32e1d63f4ce50451f
panayotsky.dev <panayotsky.dev@gmail.com>
2023-01-30 17:13:39 +0200

    Update Loginform.jsx

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Card, Input, Form, Button } from './form'
-import styled from 'styled-components'
+
 
 
 
 export default function Loginform() {
   return (
-    <Card>
-      <Form>
-        <Input type="email" placeholder="Email"/>
-        <Input type="password" placeholder="Password"/>
-        <Button> Submit           
-        </Button>      
+    <Card class="card">
+      <Form class="form">
+        <Input class="email" type="email" placeholder="Email"/>
+        <Input class="password" type="password" placeholder="Password"/>
+        <Button class="button"> Submit </Button>      
       </Form>        
     </Card>
   )
